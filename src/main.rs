62ed7b95b2ad143e55b15efd7eb743e4a6aba192
kazuhideoki <cedar123pc@gmail.com>
2024-08-04 16:12:51 +0900

    env 読み込み

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use dotenvy::dotenv;
+use std::env;
+struct Env {
+    test: String,
+}
+impl Env {
+    fn new() -> Self {
+        dotenv().ok();
+
+        Env {
+            test: env::var("TEST").expect("TEST is not set"),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let env = Env::new();
+    println!("TEST: {}", env.test);
 }
